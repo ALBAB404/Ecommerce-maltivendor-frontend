99e64a0317dead54c388ee49075b3e9863372ba7
ALBAB404 <imalbab4@gmail.com>
2023-09-13 18:19:53 +0600

    templating done

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as cartSideBar } from "./cartSideBar.vue"; 
+export { default as footer } from "./footer.vue"; 
+export { default as headerpart } from "./headerpart.vue"; 
+export { default as mobileManu } from "./mobileManu.vue"; 
+export { default as NavBar } from "./NavBar.vue"; 
+export { default as newsLetter } from "./newsLetter.vue"; 
