09e3544bffa338c1ad9adb6cfe5b856de7857a29
ALBAB404 <imalbab4@gmail.com>
2023-09-14 12:26:27 +0600

    dynamic title done

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,9 @@ import Index from '../views/pages/index.vue'
 import {UserLogin, UserRegister} from '@/views/auth'
 
 const routes = [
-    { path: '/', name: 'index.page', component: Index },
-    { path: '/auth/login', name: 'user.login',  component: UserLogin },
-    { path: '/auth/register', name: 'user.register',  component: UserRegister },
+    { path: '/', name: 'index.page', component: Index, meta: {title: 'Home'}, },
+    { path: '/auth/login', name: 'user.login',  component: UserLogin , meta: {title: 'Login'},},
+    { path: '/auth/register', name: 'user.register',  component: UserRegister, meta: {title: 'Register'}, },
   ]
 
 
@@ -13,5 +13,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
+
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+})
 
 export default router;
