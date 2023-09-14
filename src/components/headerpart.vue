725038612ef531b63fef76a437e739fde4455d56
ALBAB404 <imalbab4@gmail.com>
2023-09-14 12:03:30 +0600

    lgoin register routing done

--- src/components/headerpart.vue
+++ src/components/headerpart.vue
@@ -52,23 +52,23 @@
               <div class="header-content">
                 <div class="header-media-group">
                   <button class="header-user" @click="menu()">
-                    <img src="@/assets/images/menu.png" alt="user" /></button><a href="index.html"><img src="@/assets/images/logo.png"
-                      alt="logo" /></a><button class="header-src" @click="search()"><i class="fas fa-search"></i></button>
+                    <img src="@/assets/images/menu.png" alt="user" /></button><router-link :to="{name: 'index.page'}"><img src="@/assets/images/logo.png"
+                      alt="logo" /></router-link><button class="header-src" @click="search()"><i class="fas fa-search"></i></button>
                 </div>
-                <a href="index.html" class="header-logo"><img src="@/assets/images/logo.png" alt="logo" /></a>
+                <router-link :to="{name: 'index.page'}" class="header-logo"><img src="@/assets/images/logo.png" alt="logo" /></router-link>
         
                 <form class="header-form">
                   <input type="text" placeholder="Search anything..." /><button>
                     <i class="fas fa-search"></i>
                   </button>
                 </form>
-                <div class="header-widget-group">
+                <div class="header-widget-group hover-nav">
                   <li class="nav-item dropdown">
                     <a class="nav-link header-widget" href="#" data-bs-toggle="dropdown"><i class="fas fa-user"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end">
-                      <li><a href="login.html" class="dropdown-item"> Login</a></li>
+                      <li><router-link :to="{name: 'user.login'}" class="dropdown-item"> Login</router-link></li>
                       <li>
-                        <a href="register.html" class="dropdown-item"> Register</a>
+                        <router-link :to="{name: 'user.register'}" class="dropdown-item"> Register</router-link>
                       </li>
                     </ul>
                   </li>
@@ -83,3 +83,18 @@
           </header>
     </div>
 </template>
+
+<style>
+.hover-nav .nav-item .dropdown-menu {
+  display: none;
+  margin-top: 0;
+}
+
+.hover-nav .nav-item:hover .dropdown-menu {
+  display: block;
+}
+</style>
+
+<style>
+  @import "@/assets/css/user-auth.css";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Index from '../views/pages/index.vue'
+import {UserLogin, UserRegister} from '@/views/auth'
 
 const routes = [
-    { path: '/',  component: Index },
+    { path: '/', name: 'index.page', component: Index },
+    { path: '/auth/login', name: 'user.login',  component: UserLogin },
+    { path: '/auth/register', name: 'user.register',  component: UserRegister },
   ]
 
 
